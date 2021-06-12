73d432b91e7b4a50133e4121faf0fedcca051113
JoePeterson51 <>
2021-06-12 11:45:08 -0600

    Add destroy functionality

---
+++ spec/features/discounts/destroy_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'discounts destroy' do
+  it 'shows the discounts and their information' do
+    merchant = Merchant.create!(name: 'Hair Care')
+    discount_1 = merchant.discounts.create!(percentage: 20, quantity_required: 10)
+
+    visit "/merchant/#{merchant.id}/discounts"
+
+    within '#merchant-discounts' do
+      expect(page).to have_content("20%")
+      expect(page).to have_content("10 Items required for discount")
+    end
+
+    click_link "Delete Discount"
+
+    expect(current_path).to eq("/merchant/#{merchant.id}/discounts")
+
+     within '#merchant-discounts' do
+      expect(page).to_not have_content("20%")
+      expect(page).to_not have_content("10 Items required for discount")
+    end
+  end
+end
